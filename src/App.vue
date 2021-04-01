f5547a738a992fe101673875eb20edb8432ba8f1
Rose Jin <1358866738@qq.com>
2021-04-02 03:34:52 +0800

    模拟登录

--- src/App.vue
+++ src/App.vue
@@ -1,66 +1,43 @@
 <template>
-  <div id="app">
-    <h3>案例：折叠面板</h3>
-    <div>
-      <div class="title">
-        <h4>静夜思</h4>
-        <span class="btn" @click='isShow=!isShow'>
-          {{isShow?'收起':'展开'}}
-        </span>
-      </div>
-      <div class="container" v-show='isShow'>
-        <p>床前明月光，</p>
-        <p>疑是地上霜。</p>
-        <p>举头望明月，</p>
-        <p>低头思故乡。</p>
-      </div>
-    </div>
+  <div>
+    <h1>模拟登录</h1>
+    用户名：<input type="text" placeholder="请输入用户名" v-model='username'><br>
+    密码：<input type="text" placeholder="请输入密码" v-model="password">
+    <br>
+    <button @click.prevent='login'>登录</button>
+    <button @click='reset'>重置</button>
   </div>
 </template>
 
 <script>
 export default {
-  data(){
-    return{
-    isShow:true
+data(){
+  return{
+    username:'',
+    password:''
   }
+},
+methods:{
+  login(){
+    if(!this.username){
+      alert('用户名不能为空')
+    }
+    if(!this.password){
+      alert('密码不能为空')
+    }
+    console.log('需要送请求',this.username,this.password);
+  },
+  reset(){
+    this.name='',
+    this.password=''
   }
 }
+  
+    }
+  
+
 </script>
 
-<style lang="less">
-body {
-  background-color: #ccc;
-  #app {
-    width: 400px;
-    margin: 20px auto;
-    background-color: #fff;
-    border: 4px solid blueviolet;
-    border-radius: 1em;
-    box-shadow: 3px 3px 3px rgba(0, 0, 0, 0.5);
-    padding: 1em 2em 2em;
-    h3 {
-      text-align: center;
-    }
-    .title {
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
-      border: 1px solid #ccc;
-      padding: 0 1em;
-    }
-    .title h4 {
-      line-height: 2;
-      margin: 0;
-    }
-    .container {
-      border: 1px solid #ccc;
-      padding: 0 1em;
-    }
-    .btn {
-      /* 鼠标改成手的形状 */
-      cursor: pointer;
-    }
-  }
-}
+<style>
+
 </style>
